dfff80e59f5acd6afd939af0d0379ebbb441d797
Lexie McIsaac <armcisaac@gmail.com>
2024-01-04 16:47:47 -0800

    Finished functioning version of dashboard, may want to beautify

--- calculate_params.py
+++ calculate_params.py
@@ -4,140 +4,182 @@ from rdkit.Chem import AllChem
 from rdkit.Chem import rdMolTransforms
 from openff.toolkit import Molecule, ForceField, Topology
 import json
-
-# Collecting data for all molecules
-bond_data_dict = {}
-angle_data_dict = {}
-proper_data_dict = {}
-improper_data_dict = {}
-qm_data_dict = {}
-mm_data_dict = {}
-
-qm_file = 'qm_test.sdf'
-qm_mol = Molecule(qm_file)
-qm_mol_rd = Chem.SDMolSupplier(qm_file,removeHs=False)[0]
-qm_conf = qm_mol_rd.GetConformer()
-
-mm_file = 'mm_test.sdf'
-mm_mol = Molecule(mm_file)
-mm_mol_rd = Chem.SDMolSupplier(mm_file,removeHs=False)[0]
-mm_conf = mm_mol_rd.GetConformer()
+import tqdm
+import sys
 
 
-recordid = qm_mol_rd.GetPropsAsDict()['Record QCArchive']
-smiles = qm_mol_rd.GetPropsAsDict()['SMILES QCArchive']
-sage = ForceField('openff-2.1.0.offxml')
+def add_mol_to_dict(qm_file,mm_file,ff,bond_data_dict,angle_data_dict,proper_data_dict,improper_data_dict):
+    '''
+    Function to enumerate FF parameters assigned to a molecule (e.g. bond length, angle, dihedral angle),
+    calculate the actual value from both QM and MM, and compile a set of parameter dictionaries for plotting.
 
-topo = Topology.from_molecules([qm_mol])
-molecule_force_list = sage.label_molecules(topo)
-bond_dict = dict(molecule_force_list[0]['Bonds'])
-angle_dict = dict(molecule_force_list[0]['Angles'])
-proper_dict = dict(molecule_force_list[0]['ProperTorsions'])
-improper_dict = dict(molecule_force_list[0]['ImproperTorsions'])
-# print(bond_atom_id)
+    Inputs:
+        qm_file: SDF file with the optimized QM geometry
+        mm_file: SDF file with the optimized MM geometry
+        ff: ForceField object to assign parameters with
+        bond_data_dict: dictionary where bond lengths are enumerated. Can be empty
+        angle_data_dict: dictionary where angles are enumerated. Can be empty
+        proper_data_dict: dictionary where dihedral angles corresponding to
+                          proper torsion parameters are enumerated. Can be empty
+        improper_data_dict: dictionary where dihedral angles corresponding to
+                          improper torsion parameters are enumerated. Can be empty
+
+    Returns:
+        Nothing is returned, but data dictionaries are modified in place.
+    '''
+    qm_mol = Molecule(qm_file)
+    qm_mol_rd = Chem.SDMolSupplier(qm_file,removeHs=False)[0]
+    qm_conf = qm_mol_rd.GetConformer()
+
+    mm_mol = Molecule(mm_file)
+    mm_mol_rd = Chem.SDMolSupplier(mm_file,removeHs=False)[0]
+    mm_conf = mm_mol_rd.GetConformer()
+
+    recordid = qm_mol_rd.GetPropsAsDict()['Record QCArchive']
+    smiles = qm_mol.to_smiles(mapped=True)
+
+    topo = Topology.from_molecules([qm_mol])
+    molecule_force_list = ff.label_molecules(topo) # dictionary of forces for the molecule, keys are type of force ('Bonds', 'Angles', etc)
+    # these have the form {(atom1 idx, atom2 idx,...): FF parameter} for all bonds/angles/torsions in the molecule
+    bond_dict = dict(molecule_force_list[0]['Bonds'])
+    angle_dict = dict(molecule_force_list[0]['Angles'])
+    proper_dict = dict(molecule_force_list[0]['ProperTorsions'])
+    improper_dict = dict(molecule_force_list[0]['ImproperTorsions'])
+
+    # qm_data_dict[recordid] = {'Bonds':{},'Angles':{},'ProperTorsions':{},'ImproperTorsions':{}} # record ID: {'Bonds': (idx):qm_bl} --> can be read in to avoid recalc
+    # mm_data_dict[recordid] = {'Bonds':{},'Angles':{},'ProperTorsions':{},'ImproperTorsions':{}} # same but for MM
+
+    # Bonds
+    for idx in bond_dict:
+        b = bond_dict[idx]
+
+        # This part could potentially be eliminated by reading in QM geom data
+        qm_bl = Chem.rdMolTransforms.GetBondLength(qm_conf,idx[0],idx[1])
+        # qm_data_dict[recordid]['Bonds'][idx] = qm_bl
+
+        mm_bl = Chem.rdMolTransforms.GetBondLength(mm_conf,idx[0],idx[1]) # A
+        # mm_data_dict[recordid]['Bonds'][idx] = mm_bl
+
+        if b.smirks in bond_data_dict:
+            bond_data_dict[b.smirks]['molecules'].append(smiles)
+            bond_data_dict[b.smirks]['envs'].append(idx)
+            bond_data_dict[b.smirks]['qm_values'].append(qm_bl)
+            bond_data_dict[b.smirks]['mm_values'].append(mm_bl)
+        else:
+            bond_data_dict[b.smirks] = {'ident':b.id,
+                                        'sage_value': b.length.magnitude,
+                                        'qm_values': [qm_bl],
+                                        'mm_values': [mm_bl],
+                                        'molecules': [smiles],
+                                        'envs': [idx]}
+
+    # Angles
+    for idx in angle_dict:
+        b = angle_dict[idx]
+
+        # This part could potentially be eliminated by reading in QM geom data
+        qm_ang = Chem.rdMolTransforms.GetAngleDeg(qm_conf,idx[0],idx[1],idx[2])
+        # qm_data_dict[recordid]['Angles'][idx] = qm_ang
+        mm_ang = Chem.rdMolTransforms.GetAngleDeg(mm_conf,idx[0],idx[1],idx[2])
+        # mm_data_dict[recordid]['Angles'][idx] = mm_ang
+
+        if b.smirks in angle_data_dict:
+            angle_data_dict[b.smirks]['molecules'].append(smiles)
+            angle_data_dict[b.smirks]['envs'].append(idx)
+            angle_data_dict[b.smirks]['qm_values'].append(qm_ang)
+            angle_data_dict[b.smirks]['mm_values'].append(mm_ang)
+        else:
+            angle_data_dict[b.smirks] = {'ident':b.id,
+                                        'sage_value': b.angle.magnitude,
+                                        'qm_values': [qm_ang],
+                                        'mm_values': [mm_ang],
+                                        'molecules': [smiles],
+                                        'envs': [idx]}
+
+    # Proper Torsions
+    for idx in proper_dict:
+        b = proper_dict[idx]
+
+        # This part could potentially be eliminated by reading in QM geom data
+        qm_tor = Chem.rdMolTransforms.GetDihedralDeg(qm_conf,idx[0],idx[1],idx[2],idx[3])
+        # qm_data_dict[recordid]['ProperTorsions'][idx] = qm_tor
+        mm_tor = Chem.rdMolTransforms.GetDihedralDeg(mm_conf,idx[0],idx[1],idx[2],idx[3])
+        # mm_data_dict[recordid]['ProperTorsions'][idx] = mm_tor
+
+        if b.smirks in proper_data_dict:
+            proper_data_dict[b.smirks]['molecules'].append(smiles)
+            proper_data_dict[b.smirks]['envs'].append(idx)
+            proper_data_dict[b.smirks]['qm_values'].append(qm_tor)
+            proper_data_dict[b.smirks]['mm_values'].append(mm_tor)
+        else:
+            proper_data_dict[b.smirks] = {'ident':b.id,
+                                        # 'sage_value': b.angle.magnitude,
+                                        'qm_values': [qm_tor],
+                                        'mm_values': [mm_tor],
+                                        'molecules': [smiles],
+                                        'envs': [idx]}
+
+    # Improper Torsions
+    for idx in improper_dict:
+        b = improper_dict[idx]
+
+        # This part could potentially be eliminated by reading in QM geom data
+        qm_tor = Chem.rdMolTransforms.GetDihedralDeg(qm_conf,idx[0],idx[1],idx[2],idx[3])
+        # qm_data_dict[recordid]['ProperTorsions'][idx] = qm_tor
+        mm_tor = Chem.rdMolTransforms.GetDihedralDeg(mm_conf,idx[0],idx[1],idx[2],idx[3])
+        # mm_data_dict[recordid]['ProperTorsions'][idx] = mm_tor
+
+        if b.smirks in improper_data_dict:
+            improper_data_dict[b.smirks]['molecules'].append(smiles)
+            improper_data_dict[b.smirks]['envs'].append(idx)
+            improper_data_dict[b.smirks]['qm_values'].append(qm_tor)
+            improper_data_dict[b.smirks]['mm_values'].append(mm_tor)
+        else:
+            improper_data_dict[b.smirks] = {'ident':b.id,
+                                        # 'sage_value': b.angle.magnitude,
+                                        'qm_values': [qm_tor],
+                                        'mm_values': [mm_tor],
+                                        'molecules': [smiles],
+                                        'envs': [idx]}
+
+        return
+
+def main():
+    # Collecting data for all molecules
+    bond_data_dict = {}
+    angle_data_dict = {}
+    proper_data_dict = {}
+    improper_data_dict = {}
+    qm_data_dict = {}
+    mm_data_dict = {}
+
+    # TODO: make this modifiable easily
+    dir = '/Users/lexiemcisaac/Documents/OpenFF/conformer_energy_ordering/swope_scripts/benchmarking/'
+    compound_list = dir + 'all_molecules.txt'
+    qm_dir = dir + 'b3lyp-d3bj_dzvp/'
+    mm_dir = dir + 'openff-2.1.0/'
+    sage = ForceField('openff-2.1.0.offxml')
+    # jsonpath = 'test_bond2.json'
+
+    all_mols = np.loadtxt(compound_list,dtype='str')[:50]
+    # print(all_mols)
+    for mol in tqdm.tqdm(all_mols,desc='Calculating geometric parameters'):
+        qm_file = qm_dir + mol
+        mm_file = mm_dir + mol
+        add_mol_to_dict(qm_file,mm_file,sage,bond_data_dict,angle_data_dict,proper_data_dict,improper_data_dict)
+
+    with open('bonds_test2.json','w') as jsonfile:
+        json.dump(bond_data_dict,jsonfile,indent=4)
+
+    with open('angles_test2.json','w') as jsonfile:
+        json.dump(angle_data_dict,jsonfile,indent=4)
+
+    with open('propers_test2.json','w') as jsonfile:
+        json.dump(proper_data_dict,jsonfile,indent=4)
+
+    with open('impropers_test2.json','w') as jsonfile:
+        json.dump(improper_data_dict,jsonfile,indent=4)
 
 
-qm_data_dict[recordid] = {'Bonds':{},'Angles':{},'ProperTorsions':{},'ImproperTorsions':{}} # record ID: {'Bonds': (idx):qm_bl} --> can be read in to avoid recalc
-mm_data_dict[recordid] = {'Bonds':{},'Angles':{},'ProperTorsions':{},'ImproperTorsions':{}} # same but for MM
-
-# Bonds
-for idx in bond_dict:
-    b = bond_dict[idx]
-
-    # This part could potentially be eliminated by reading in QM geom data
-
-    qm_bl = Chem.rdMolTransforms.GetBondLength(qm_conf,idx[0],idx[1])
-    qm_data_dict[recordid]['Bonds'][idx] = qm_bl
-
-    mm_bl = Chem.rdMolTransforms.GetBondLength(mm_conf,idx[0],idx[1]) # A
-    mm_data_dict[recordid]['Bonds'][idx] = mm_bl
-
-    if b.smirks in bond_data_dict:
-        bond_data_dict[b.smirks]['molecules'].append(smiles)
-        bond_data_dict[b.smirks]['envs'].append(idx)
-        bond_data_dict[b.smirks]['qm_values'].append(qm_bl)
-        bond_data_dict[b.smirks]['mm_values'].append(mm_bl)
-    else:
-        bond_data_dict[b.smirks] = {'ident':b.id,
-                                    'sage_value': b.length.magnitude,
-                                    'qm_values': [qm_bl],
-                                    'mm_values': [mm_bl],
-                                    'molecules': [smiles],
-                                    'envs': [idx]}
-
-
-# print(bond_data_dict)
-
-# Angles
-for idx in angle_dict:
-    b = angle_dict[idx]
-
-    # This part could potentially be eliminated by reading in QM geom data
-    qm_ang = Chem.rdMolTransforms.GetAngleDeg(qm_conf,idx[0],idx[1],idx[2])
-    qm_data_dict[recordid]['Angles'][idx] = qm_ang
-    mm_ang = Chem.rdMolTransforms.GetAngleDeg(mm_conf,idx[0],idx[1],idx[2])
-    mm_data_dict[recordid]['Angles'][idx] = mm_ang
-
-    if b.smirks in angle_data_dict:
-        angle_data_dict[b.smirks]['molecules'].append(smiles)
-        angle_data_dict[b.smirks]['envs'].append(idx)
-        angle_data_dict[b.smirks]['qm_values'].append(qm_ang)
-        angle_data_dict[b.smirks]['mm_values'].append(mm_ang)
-    else:
-        angle_data_dict[b.smirks] = {'ident':b.id,
-                                    'sage_value': b.angle.magnitude,
-                                    'qm_values': [qm_ang],
-                                    'mm_values': [mm_ang],
-                                    'molecules': [smiles],
-                                    'envs': [idx]}
-# print(angle_data_dict)
-
-# Proper Torsions
-for idx in proper_dict:
-    b = proper_dict[idx]
-
-    # This part could potentially be eliminated by reading in QM geom data
-    qm_tor = Chem.rdMolTransforms.GetDihedralDeg(qm_conf,idx[0],idx[1],idx[2],idx[3])
-    qm_data_dict[recordid]['ProperTorsions'][idx] = qm_tor
-    mm_tor = Chem.rdMolTransforms.GetDihedralDeg(mm_conf,idx[0],idx[1],idx[2],idx[3])
-    mm_data_dict[recordid]['ProperTorsions'][idx] = mm_tor
-
-    if b.smirks in proper_data_dict:
-        proper_data_dict[b.smirks]['molecules'].append(smiles)
-        proper_data_dict[b.smirks]['envs'].append(idx)
-        proper_data_dict[b.smirks]['qm_values'].append(qm_tor)
-        proper_data_dict[b.smirks]['mm_values'].append(mm_tor)
-    else:
-        proper_data_dict[b.smirks] = {'ident':b.id,
-                                    # 'sage_value': b.angle.magnitude,
-                                    'qm_values': [qm_tor],
-                                    'mm_values': [mm_tor],
-                                    'molecules': [smiles],
-                                    'envs': [idx]}
-# print(proper_data_dict)
-
-# Improper Torsions
-for idx in improper_dict:
-    b = improper_dict[idx]
-
-    # This part could potentially be eliminated by reading in QM geom data
-    qm_tor = Chem.rdMolTransforms.GetDihedralDeg(qm_conf,idx[0],idx[1],idx[2],idx[3])
-    qm_data_dict[recordid]['ProperTorsions'][idx] = qm_tor
-    mm_tor = Chem.rdMolTransforms.GetDihedralDeg(mm_conf,idx[0],idx[1],idx[2],idx[3])
-    mm_data_dict[recordid]['ProperTorsions'][idx] = mm_tor
-
-    if b.smirks in improper_data_dict:
-        improper_data_dict[b.smirks]['molecules'].append(smiles)
-        improper_data_dict[b.smirks]['envs'].append(idx)
-        improper_data_dict[b.smirks]['qm_values'].append(qm_tor)
-        improper_data_dict[b.smirks]['mm_values'].append(mm_tor)
-    else:
-        improper_data_dict[b.smirks] = {'ident':b.id,
-                                    # 'sage_value': b.angle.magnitude,
-                                    'qm_values': [qm_tor],
-                                    'mm_values': [mm_tor],
-                                    'molecules': [smiles],
-                                    'envs': [idx]}
-# print(improper_data_dict)
-with open('test_bond.json','w') as jsonfile:
-    json.dump(bond_data_dict,jsonfile,indent=4)
+if __name__ == '__main__':
+    main()
--- dashboard.py
+++ dashboard.py
@@ -4,89 +4,316 @@ import pandas as pd
 import plotly.express as px
 import json
 import plotly.graph_objects as go
+import numpy as np
+# For drawing:
+from openff.toolkit.topology import FrozenMolecule
+from openff.toolkit import Molecule
+from rdkit import Chem
+from rdkit.Chem import Draw
+from rdkit.Chem import Descriptors
+from rdkit.Chem.rdDepictor import Compute2DCoords
+from IPython.display import SVG
+import base64
 
-with open('angles_test2_rnmm.json','r') as jsonfile:
-    test = dict(json.load(jsonfile))
 
-test_keys = list(test.keys())
-test_df = pd.DataFrame(test[test_keys[0]])
+DEFAULT_WIDTH = int(980/2)
+DEFAULT_HEIGHT = int(400/2)
+def draw_molecule_svg(
+    molecule,
+    image_width = DEFAULT_WIDTH,
+    image_height= DEFAULT_HEIGHT,
+    highlight_atoms= None,
+    highlight_bonds = None,
+    atom_notes = None,
+    bond_notes = None,
+    explicit_hydrogens = None,
+    color_by_element = None,
+):
+    """Draw a molecule--stolen from OpenFF docs
+    Parameters
+    ==========
+    molecule
+        The molecule to draw.
+    image_width
+        The width of the resulting image in pixels.
+    image_height
+        The height of the resulting image in pixels.
+    highlight_atoms
+        A list of atom indices to highlight, or a map from indices to colors.
+        Colors should be given as triplets of floats between 0.0 and 1.0.
+    highlight_atoms
+        A list of pairs of atom indices indicating bonds to highlight, or a map
+        from index pairs to colors. Colors should be given as triplets of floats
+        between 0.0 and 1.0.
+    atom_notes
+        A map from atom indices to a string that should be printed near the
+        atom.
+    bond_notes
+        A map from atom index pairs to a string that should be printed near the
+        bond.
+    explicit_hydrogens
+        If ``False``, allow uncharged monovalent hydrogens to be hidden. If
+        ``True``, make all hydrogens explicit. If ``None``
+    color_by_element
+        If True, color heteroatoms according to their element; if False, the
+        image will be black and white. By default, uses black and white when
+        highlight_atoms or highlight_bonds is provided, and color otherwise.
+    Raises
+    ======
+    KeyError
+        When an atom or bond in highlight_atoms or highlight_bonds is missing
+        from the image, including when it is present in the molecule but hidden.
+    """
+    if isinstance(molecule, FrozenMolecule):
+        rdmol = molecule.to_rdkit()
+    else:
+        rdmol = molecule
+
+    if color_by_element is None:
+        color_by_element = highlight_atoms is None and highlight_bonds is None
+
+    if explicit_hydrogens is None:
+        idx_map = {i: i for i in range(rdmol.GetNumAtoms())}
+    elif explicit_hydrogens:
+        idx_map = {i: i for i in range(rdmol.GetNumAtoms())}
+        rdmol = Chem.AddHs(rdmol, explicitOnly=True)
+    else:
+        idx_map = {
+            old: new
+            for new, old in enumerate(
+                a.GetIdx()
+                for a in rdmol.GetAtoms()
+                if a.GetAtomicNum() != 1 and a.GetMass() != 1
+            )
+        }
+        rdmol = Chem.RemoveHs(rdmol, updateExplicitCount=True)
+
+    if highlight_atoms is None:
+        highlight_atoms = []
+        highlight_atom_colors = None
+    elif isinstance(highlight_atoms, dict):
+        highlight_atom_colors = {
+            idx_map[i]: tuple(c) for i, c in highlight_atoms.items() if i in idx_map
+        }
+        highlight_atoms = list(highlight_atoms.keys())
+    else:
+        highlight_atoms = [idx_map[i] for i in highlight_atoms if i in idx_map]
+        highlight_atom_colors = None
+
+    if highlight_bonds is None:
+        highlight_bonds = []
+        highlight_bond_colors = None
+    elif isinstance(highlight_bonds, dict):
+        highlight_bond_colors = {
+            rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b]).GetIdx(): tuple(v)
+            for (i_a, i_b), v in highlight_bonds.items()
+            if i_a in idx_map and i_b in idx_map
+        }
+
+        highlight_bonds = list(highlight_bond_colors.keys())
+    else:
+        highlight_bonds = [
+            rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b])
+            for i_a, i_b in highlight_bonds
+            if i_a in idx_map and i_b in idx_map
+        ]
+        highlight_bond_colors = None
+
+    if bond_notes is not None:
+        for (i_a, i_b), note in bond_notes.items():
+            if i_a not in idx_map or i_b not in idx_map:
+                continue
+            rdbond = rdmol.GetBondBetweenAtoms(idx_map[i_a], idx_map[i_b])
+            rdbond.SetProp("bondNote", note)
+
+    if atom_notes is not None:
+        for i, note in atom_notes.items():
+            if i not in idx_map:
+                continue
+            rdatom = rdmol.GetAtomWithIdx(idx_map[i])
+            rdatom.SetProp("atomNote", note)
+
+    Compute2DCoords(rdmol)
+
+    drawer = Draw.MolDraw2DSVG(image_width, image_height)
+
+    draw_options = drawer.drawOptions()
+    if not color_by_element:
+        draw_options.useBWAtomPalette()
+
+    drawer.DrawMolecule(
+        rdmol,
+        highlightAtoms=highlight_atoms,
+        highlightAtomColors=highlight_atom_colors,
+        highlightBonds=highlight_bonds,
+        highlightBondColors=highlight_bond_colors,
+    )
+    drawer.FinishDrawing()
+
+    svg_contents = drawer.GetDrawingText()
+
+    return svg_contents
+
+@callback(
+   Output('click-output', 'children',allow_duplicate=True),
+   Input('controls-and-graph','clickData'),
+   Input('param-id',component_property='value'),
+   Input('data_type',component_property='value'),
+   prevent_initial_call=True
+)
+def draw_mols(clickData,fig_title,dtype):
+    global JSONS
+    json_file = JSONS[dtype]
+    key = fig_title.split(': ')[1]
+    try:
+        idx = clickData['points'][0]['pointIndex']
+        atom_idx = json_file[key]['envs'][idx]
+        smiles = json_file[key]['molecules'][idx]
+
+        svg = draw_molecule_svg(Molecule.from_mapped_smiles(smiles),highlight_atoms=atom_idx)
+
+        # Took this part from Brent
+        try:
+            encoded = base64.b64encode(bytes(svg, "utf-8"))
+        except Exception as e:
+            print("error: ", e)
+        pic = html.Img(src=f"data:image/svg+xml;base64,{encoded.decode()}")
+        return pic
+    except TypeError:
+        pass
 
 @callback(
     Output(component_id = 'controls-and-graph',component_property='figure'),
-    Input(component_id = 'param-id',component_property='value')
+    Input(component_id = 'param-id',component_property='value'),
+    Input(component_id = 'data_type',component_property='value')
 )
-def make_figure(key):
-    df = pd.DataFrame(test[key])
+def make_figure(fig_title,dtype):
+    global JSONS
+    json = JSONS[dtype]
+    key = fig_title.split(': ')[1]
+    df = pd.DataFrame(json[key])
+    if dtype == 'Bonds':
+        unit = 'A'
+    else:
+        unit = 'deg'
+
     layout = go.Layout(
-        title=df['ident'][0] +': '+ key,
+        title=fig_title,
         xaxis=dict(
-            title="MM value"
+            title="MM value ({})".format(unit)
         ),
         yaxis=dict(
-            title="QM value"
+            title="QM value ({})".format(unit)
     ) )
+
     fig = go.Figure(layout=layout)
     fig.add_trace(go.Scatter(x = [df['qm_values'].min(),df['qm_values'].max()],y = [df['qm_values'].min(),df['qm_values'].max()],mode='lines',line=dict(color='black',dash='dash'),name='x=y'))
-
-    fig.add_trace(go.Scatter(x = df['espaloma_values'], y = df['qm_values'],mode='markers',marker=dict(color='blue')))
-    fig.add_trace(go.Scatter(x = [df['sage_value'][0]], y = [df['sage_value'][0]],mode='markers', marker=dict(size=[15],color='red'),marker_symbol='star',name='Sage'))
+    fig.add_trace(go.Scatter(x = df['mm_values'], y = df['qm_values'],mode='markers',marker=dict(color='blue'),name='Data'))
+    try:
+        fig.add_trace(go.Scatter(x = [df['sage_value'][0]], y = [df['sage_value'][0]],mode='markers', marker=dict(size=[15],color='red'),marker_symbol='star',name='Sage'))
+    except KeyError:
+        pass
 
     return fig
 
 
-# @callback(
-#     [
-#         Output("graph-container", "children", allow_duplicate=True),
-#         Output("smirks_input", "value", allow_duplicate=True),
-#     ],
-#     Input("previous", "n_clicks"),
-#     prevent_initial_call=True,
-# )
-# def previous_button(_):
-#     global CUR_SMIRK
-#     if CUR_SMIRK >= 1:
-#         CUR_SMIRK -= 1
-#     return make_fig(cur_record(), NCLUSTERS), SMIRKS[CUR_SMIRK]
+######
 #
+# Defining the global inputs
 #
-# @callback(
-#     [
-#         Output("graph-container", "children", allow_duplicate=True),
-#         Output("smirks_input", "value", allow_duplicate=True),
-#     ],
-#     Input("next", "n_clicks"),
-#     prevent_initial_call=True,
-# )
-# def next_button(_):
-#     global CUR_SMIRK
-#     if CUR_SMIRK < len(SMIRKS) - 1:
-#         CUR_SMIRK += 1
-#     return make_fig(cur_record(), NCLUSTERS), SMIRKS[CUR_SMIRK]
+######
+
+# First load in all data so it's available easily for toggling
+BOND_DATA_FILE='bonds_test2.json'
+ANGLE_DATA_FILE='angles_test2.json'
+PROPER_DATA_FILE='propers_test2.json'
+IMPROPER_DATA_FILE='impropers_test2.json'
+
+with open(BOND_DATA_FILE,'r') as jsonfile:
+    BOND_JSON = dict(json.load(jsonfile))
+
+with open(ANGLE_DATA_FILE,'r') as jsonfile:
+    ANGLE_JSON = dict(json.load(jsonfile))
+
+with open(PROPER_DATA_FILE,'r') as jsonfile:
+    PROPER_JSON = dict(json.load(jsonfile))
+
+with open(IMPROPER_DATA_FILE,'r') as jsonfile:
+    IMPROPER_JSON = dict(json.load(jsonfile))
+
+# Only JSONS winds up being needed as a global variable.
+JSONS = {'Bonds':BOND_JSON,'Angles':ANGLE_JSON,'Proper Torsions':PROPER_JSON,'Improper Torsions':IMPROPER_JSON}
+KEYS = {'Bonds':list(BOND_JSON.keys()),'Angles':list(ANGLE_JSON.keys()),'Proper Torsions':list(PROPER_JSON.keys()),'Improper Torsions':list(IMPROPER_JSON.keys())}
+
+PARAM_IDS = {'Bonds': [JSONS['Bonds'][k]['ident'] for k in KEYS['Bonds']],
+             'Angles': [JSONS['Angles'][k]['ident'] for k in KEYS['Angles']],
+             'Proper Torsions': [JSONS['Proper Torsions'][k]['ident'] for k in KEYS['Proper Torsions']],
+             'Improper Torsions': [JSONS['Improper Torsions'][k]['ident'] for k in KEYS['Improper Torsions']]}
 
 
+#####
+#
+# Dashboard code begins
+#
+#####
 
+# Set up connection between parameters available in dropdown menu and the type of data being displayed
+all_label_options = {
+    'Bonds': [PARAM_IDS['Bonds'][i] + ': '+KEYS['Bonds'][i] for i in range(0,len(PARAM_IDS['Bonds']))],
+    'Angles': [PARAM_IDS['Angles'][i] + ': '+KEYS['Angles'][i] for i in range(0,len(PARAM_IDS['Angles']))],
+    'Proper Torsions': [PARAM_IDS['Proper Torsions'][i] + ': '+KEYS['Proper Torsions'][i] for i in range(0,len(PARAM_IDS['Proper Torsions']))],
+    'Improper Torsions': [PARAM_IDS['Improper Torsions'][i] + ': '+KEYS['Improper Torsions'][i] for i in range(0,len(PARAM_IDS['Improper Torsions']))]
+}
 
+@callback(
+    Output('param-id','options'),
+    Output('param-id','value'),
+    Input('data_type','value')
+)
+def set_paramid(data_type):
+    return all_label_options[data_type], all_label_options[data_type][0]
 
+# Set up dashboard
 app = Dash(__name__)
-
 app.layout = html.Div([
-    html.Div(children='QM vs MM geometry parameters'),
-
-    dcc.Graph(figure=make_figure(test_keys[0]), id = 'controls-and-graph'),
-    html.Div([
+    html.H1(children='QM vs MM geometry parameters'), # Title
+    html.Div([  # Toggle buttons
 
         html.Div([
-            dcc.Dropdown(
-                test_keys,
-                'Parameter',
+            dcc.RadioItems( # Button for data type
+                    list(all_label_options.keys()),
+                    value = 'Bonds',
+                    id='data_type',
+                    labelStyle={'display': 'inline-block', 'marginTop': '5px'}
+            ),
+
+            dcc.Dropdown( # Dropdown for parameter selection
+                # options=[],
+                # value=all_label_options['Bonds'][0],
                 id='param-id'
             )
-        ])
-    ])
-    # html.Button("Previous", id="previous", n_clicks=0),
-    # html.Button("Next", id="next", n_clicks=0),
+        ],style={'width': '49%', 'display': 'inline-block'})
+    ]),
+
+    html.Div([  # Graph/structures
+        html.Div(  # Graph (will auto-update based on param ID chosen from dropdown)
+            dcc.Graph(figure={}, id = 'controls-and-graph'),
+            style={'width': '48%', 'padding': '0px 20px 20px 20px','display':'inline-block',}
+        ),
+        html.Div(  # Draw structures
+            [],
+            id="click-output",
+            style={'width': '40%', 'padding': '0px 20px 20px 20px', 'display':'inline-block',
+                "max-height": "90vh",
+                "overflow": "hidden",
+                "overflow-y": "scroll",
+            },
+        )
+    ]),
 
 ])
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
